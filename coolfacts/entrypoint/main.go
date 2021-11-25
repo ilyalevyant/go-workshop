9803465331bbaef24f363e13803e78e0f0f3a89c
ilyalevyant <eliyahulevyant@gmail.com>
2021-11-25 11:10:12 +0200

    c

--- coolfacts/entrypoint/main.go
+++ coolfacts/entrypoint/main.go
@@ -3,23 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
+ 	"github.com/FTBpro/go-workshop/coolfacts/entrypoint/fact"
 )
 
 func main() {
 	//app.StartApplication()
 
 	//fmt.Println("Welcome to the go-workshop, hope you will have fun!!")
-	facts := repo{}
-	facts.add(fact{
-		Image:       "image1",
-		Description: "description1",
-	})
-	facts.add(fact{
-		Image:       "image2",
-		Description: "description2",
-	})
+
+	facts, err := mentalfloss{}.Facts()
+	if err != nil {
+		fmt.Printf("can't reach mentalfloss: ", err)
+	}
+
+	//facts.add(fact{
+	//	Image:       "https://images2.minutemediacdn.com/image/upload/v1556645500/shape/cover/entertainment/D5aliXvWsAEcYoK-fe997566220c082b98030508e654948e.jpg",
+	//	Description: "Did you know sonic is a hedgehog?!",
+	//})
+	//facts.add(fact{
+	//	Image:       "https://images2.minutemediacdn.com/image/upload/v1556641470/shape/cover/entertainment/uncropped-Screen-Shot-2019-04-30-at-122411-PM-3b804f143c543dfab4b75c81833bed1b.jpg",
+	//	Description: "You won't believe what happened to Arya!",
+	//})
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet{
@@ -36,13 +43,27 @@ func main() {
 	http.HandleFunc("/facts", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			factsList := facts.getAll()
-			result, err := json.Marshal(factsList)
+			//factsList := facts.getAll()
+			//result, err := json.Marshal(factsList)
+			//if err != nil{
+			//	http.Error(w, err.Error(), http.StatusInternalServerError)
+			//	return
+			//}
+			//w.Write(result)
+			tmpl, err := template.New("facts").Parse(newsTemplate)
 			if err != nil{
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			w.Write(result)
+			//facts := facts.getAll()
+			err = tmpl.Execute(w, facts)
+			if err != nil{
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+
+
 		case http.MethodPost:
 			bodyRequest, err := ioutil.ReadAll(r.Body)
 			if err != nil{
@@ -54,11 +75,11 @@ func main() {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
-			newFact := fact{
+			newFact := fact.Fact{
 				Image:       factRequest.Image,
 				Description: factRequest.Description,
 			}
-			facts.add(newFact)
+			facts = append(facts, newFact)
 			w.Write([]byte("success"))
 		default:
 			http.Error(w, "page is not found", http.StatusNotFound)
@@ -66,8 +87,7 @@ func main() {
 		}
 	})
 
-
-
 	http.ListenAndServe(":9002", nil)
-
 }
+
+
